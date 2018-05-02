01f746b9db4837a40a2a76ed22000499d7d00777
Garrett Evers <grevers@bsu.edu>
2018-05-02 08:42:19 -0400

    Updated views and data

--- App.js
+++ App.js
@@ -1,72 +1,14 @@
 import React from 'react';
-import { StyleSheet, Button, Text, View, TouchableOpacity } from 'react-native';
-import { BarCodeScanner, Permissions } from 'expo';
+import { StyleSheet, View} from 'react-native';
+import Router from './src/Router';
+
 
 export default class App extends React.Component {
-  state = {
-    cameraOn: false,
-    hasCameraPermission: null,
-    qrcode: '',
-  }
-
-  async componentWillMount() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({hasCameraPermission: status === 'granted'});
-  }
-
-  _handleBarCodeRead = ({ type, data }) => {
-    //alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-    this.setState({qrcode: data})
-  }
-
-  toggleCamera = () => {
-    this.setState({cameraOn : !this.state.cameraOn});
-  }
-
-  renderCamera = (hasCameraPermission) => {
-    if (hasCameraPermission === null) {
-      return <Text>Requesting for camera permission</Text>;
-    } else if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>;
-    } else {
-      return (
-        <View>
-          <BarCodeScanner
-            onBarCodeRead={this._handleBarCodeRead}
-            style={StyleSheet.absoluteFill}
-          />
-          <Text style={{
-               backgroundColor: 'white'
-           }}>{this.state.qrcode}</Text>
-        </View>
-      );
-    }
-  }
-
   render() {
-    const { hasCameraPermission } = this.state;
-    return(
-      <View style={styles.container}>
-        <Button
-          title="I button"
-          style={styles.preview}
-          onPress={this.toggleCamera}>
-        </Button>
-        {this.state.cameraOn ? this.renderCamera(hasCameraPermission) : <Text> Hi </Text>}
+    return (
+      <View style={{ flex: 1 }}>
+        <Router />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 30,
-    flexDirection: 'column',
-  },
-  preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center'
-  }
-});
